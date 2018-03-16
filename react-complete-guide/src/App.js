be6bc2dd57e25ea7445e17e08d0f8681a8042909
Moritz Jung <moritz.jung@outlook.de>
2018-03-16 23:58:17 +0100

    State and dynamic output

--- react-complete-guide/src/App.js
+++ react-complete-guide/src/App.js
@@ -3,13 +3,22 @@ import './App.css';
 import Person from './Person/Person';
 
 class App extends Component {
+    state = {
+        persons: [
+            { name: "Laura", age: 30},
+            { name: "Rafal", age: 32},
+            { name: "Robbie", age: 42},
+        ]
+    }
+
     render() {
         return (
             <div className="App">
                 <h1>Hi I am a React App</h1>
-                <Person name="Laura" age="30"/>
-                <Person name="Rafal" age="32">Hobbies: Boxing!</Person>
-                <Person name="Robbie" age="42"/>
+                <button>Switch Name</button>
+                <Person name={this.state.persons[0].name} age={this.state.persons[0].age}/>
+                <Person name={this.state.persons[1].name} age={this.state.persons[1].age}>Hobbies: Boxing!</Person>
+                <Person name={this.state.persons[2].name} age={this.state.persons[2].age}/>
             </div>
         );
     }
