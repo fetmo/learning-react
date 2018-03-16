b1109402197b3ec76d7dc4596a51c428951668eb
Moritz Jung <moritz.jung@outlook.de>
2018-03-17 00:09:06 +0100

    State change and event handling

--- react-complete-guide/src/App.js
+++ react-complete-guide/src/App.js
@@ -5,17 +5,33 @@ import Person from './Person/Person';
 class App extends Component {
     state = {
         persons: [
-            { name: "Laura", age: 30},
-            { name: "Rafal", age: 32},
-            { name: "Robbie", age: 42},
-        ]
+            {name: "Laura", age: 30},
+            {name: "Rafal", age: 32},
+            {name: "Robbie", age: 42},
+        ],
+        otherState: "some other value"
+    }
+
+    switchNameHandler = () => {
+        let index = Math.floor(Math.random() * 2);
+
+        // DONT DO THIS:
+        // this.state.persons[index].name += " Clicked";
+
+        this.setState({
+            persons: [
+                {name: "Laura Blöcher", age: 30},
+                {name: "Rafal Wesolowski", age: 32},
+                {name: "Robbie Wittrock", age: 42},
+            ]
+        })
     }
 
     render() {
         return (
             <div className="App">
                 <h1>Hi I am a React App</h1>
-                <button>Switch Name</button>
+                <button onClick={this.switchNameHandler}>Switch Name</button>
                 <Person name={this.state.persons[0].name} age={this.state.persons[0].age}/>
                 <Person name={this.state.persons[1].name} age={this.state.persons[1].age}>Hobbies: Boxing!</Person>
                 <Person name={this.state.persons[2].name} age={this.state.persons[2].age}/>
