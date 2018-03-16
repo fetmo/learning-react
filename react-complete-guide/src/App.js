9ee61b310b34f35f61c1382b5a3533fed40213f1
Moritz Jung <moritz.jung@outlook.de>
2018-03-16 21:21:38 +0100

    First custom component

--- react-complete-guide/src/App.js
+++ react-complete-guide/src/App.js
@@ -1,15 +1,16 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import './App.css';
+import Person from './Person/Person';
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <h1>Hi I am a React App</h1>
-      </div>
-    );
-    //   return React.createElement('div', {className: "App"}, React.createElement('h1', null, 'Hi I am a React App'));
-  }
+    render() {
+        return (
+            <div className="App">
+                <h1>Hi I am a React App</h1>
+                <Person/>
+            </div>
+        );
+    }
 }
 
 export default App;
