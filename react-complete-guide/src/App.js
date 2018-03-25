b390fa5ece76548aa59e251cba1f9b4e664cab3d
Moritz Jung <moritz.jung@outlook.de>
2018-03-25 16:45:30 +0200

    Radium: media queries

--- react-complete-guide/src/App.js
+++ react-complete-guide/src/App.js
@@ -1,6 +1,6 @@
 import React, {Component} from 'react';
 import './App.css';
-import Radium from 'radium';
+import Radium, {StyleRoot} from 'radium';
 import Person from './Person/Person';
 
 class App extends Component {
@@ -90,21 +90,23 @@ class App extends Component {
         if (this.state.persons.length <= 2) {
             classes.push('red');
         }
-        if (this.state.persons.length <= 1){
+        if (this.state.persons.length <= 1) {
             classes.push('bold');
         }
 
         return (
-            <div className="App">
-                <h1>Hi, I am a React App</h1>
-                <p className={classes.join(' ')}>This is really working!</p>
-                <button
-                    style={buttonstyle}
-                    onClick={this.togglePersonsHandler}>
-                    Toggle Persons
-                </button>
-                {persons}
-            </div>
+            <StyleRoot>
+                <div className="App">
+                    <h1>Hi, I am a React App</h1>
+                    <p className={classes.join(' ')}>This is really working!</p>
+                    <button
+                        style={buttonstyle}
+                        onClick={this.togglePersonsHandler}>
+                        Toggle Persons
+                    </button>
+                    {persons}
+                </div>
+            </StyleRoot>
         );
     }
 }
--- react-complete-guide/src/Person/Person.js
+++ react-complete-guide/src/Person/Person.js
@@ -3,8 +3,14 @@ import './Person.css';
 import Radium from 'radium';
 
 const person = (props) => {
+    const personStyle = {
+        '@media (min-width: 500px)': {
+            width: '450px'
+        }
+    };
+
     return (
-        <div className="Person">
+        <div className="Person" style={personStyle}>
             <p onClick={props.click}>I'm {props.name} and I am {props.age} years old!</p>
             <p>{props.children}</p>
             <input type="text" onChange={props.change} value={props.name} />
