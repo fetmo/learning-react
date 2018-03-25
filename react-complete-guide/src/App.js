acba9307d5fa58461f2f1ada7df051f3d1d425c1
Moritz Jung <moritz.jung@outlook.de>
2018-03-25 16:50:29 +0200

    React CSS Modules: pre npm eject

--- react-complete-guide/src/App.js
+++ react-complete-guide/src/App.js
@@ -1,6 +1,5 @@
 import React, {Component} from 'react';
 import './App.css';
-import Radium, {StyleRoot} from 'radium';
 import Person from './Person/Person';
 
 class App extends Component {
@@ -55,20 +54,12 @@ class App extends Component {
             border: '1px solid blue',
             padding: '8px',
             cursor: 'pointer',
-            ':hover': {
-                backgroundColor: 'lightgreen',
-                color: 'black'
-            }
         };
 
         let persons = null;
 
         if (this.state.showPersons) {
             buttonstyle.backgroundColor = 'red';
-            buttonstyle[':hover'] = {
-                backgroundColor: 'salmon',
-                color: 'black'
-            };
 
             persons = (
                 <div>
@@ -95,20 +86,18 @@ class App extends Component {
         }
 
         return (
-            <StyleRoot>
-                <div className="App">
-                    <h1>Hi, I am a React App</h1>
-                    <p className={classes.join(' ')}>This is really working!</p>
-                    <button
-                        style={buttonstyle}
-                        onClick={this.togglePersonsHandler}>
-                        Toggle Persons
-                    </button>
-                    {persons}
-                </div>
-            </StyleRoot>
+            <div className="App">
+                <h1>Hi, I am a React App</h1>
+                <p className={classes.join(' ')}>This is really working!</p>
+                <button
+                    style={buttonstyle}
+                    onClick={this.togglePersonsHandler}>
+                    Toggle Persons
+                </button>
+                {persons}
+            </div>
         );
     }
 }
 
-export default Radium(App);
+export default App;
--- react-complete-guide/src/Person/Person.js
+++ react-complete-guide/src/Person/Person.js
@@ -1,16 +1,10 @@
 import React from 'react';
 import './Person.css';
-import Radium from 'radium';
 
 const person = (props) => {
-    const personStyle = {
-        '@media (min-width: 500px)': {
-            width: '450px'
-        }
-    };
 
     return (
-        <div className="Person" style={personStyle}>
+        <div className="Person">
             <p onClick={props.click}>I'm {props.name} and I am {props.age} years old!</p>
             <p>{props.children}</p>
             <input type="text" onChange={props.change} value={props.name} />
@@ -18,4 +12,4 @@ const person = (props) => {
     )
 };
 
-export default Radium(person);
+export default person;
