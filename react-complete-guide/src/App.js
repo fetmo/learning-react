12436145f5c2eb2051068b781ac7ab9012f0a016
Moritz Jung <moritz.jung@outlook.de>
2018-03-25 12:56:22 +0200

    Handle change event inside a list object

--- react-complete-guide/src/App.js
+++ react-complete-guide/src/App.js
@@ -5,21 +5,31 @@ import Person from './Person/Person';
 class App extends Component {
     state = {
         persons: [
-            {name: "Laura", age: 30},
-            {name: "Rafal", age: 32},
-            {name: "Robbie", age: 42},
+            { id: 'asdf1', name: "Laura", age: 30},
+            { id: '2asdf', name: "Rafal", age: 32},
+            { id: 'asdf4', name: "Robbie", age: 42},
         ],
         otherState: "some other value",
         showPersons: false
     }
 
-    nameChangedHandler = (event) => {
+    nameChangedHandler = (event, id) => {
+        const personIndex = this.state.persons.findIndex( p => {
+            return p.id === id;
+        });
+
+        const person = {
+            ...this.state.persons[personIndex] //ES6
+        };
+        //const person = Object.assign({}, this.state.persons[personIndex]); //pre ES6
+
+        person.name = event.target.value;
+
+        const persons = [...this.state.persons];
+        persons[personIndex] = person;
+
         this.setState({
-            persons: [
-                {name: event.target.value, age: 22},
-                {name: "Rafal", age: 32},
-                {name: "Robbie", age: 42},
-            ]
+            persons: persons
         })
     }
 
@@ -55,8 +65,10 @@ class App extends Component {
                         return (
                             <Person
                                 click={() => this.deletePersonHandler(key)}
+                                change={(event) => this.nameChangedHandler(event, person.id)}
                                 name={person.name}
-                                age={person.age} />
+                                age={person.age}
+                                key={person.id}/>
                         );
                     })}
                 </div>
